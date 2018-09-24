960c5a673f46fce066d95e24333f88901c4431c0
LAPTOP-MM30931E\Alex <alex27.mendes@gmail.com>
2018-09-23 21:22:05 -0300

    alterações.

--- src/main/java/com/algaworks/contatos/repository/ProdutosRepository.java
+++ src/main/java/com/algaworks/contatos/repository/ProdutosRepository.java
@@ -1,9 +1,16 @@
 package com.algaworks.contatos.repository;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.contatos.model.Produto;
 
 public interface ProdutosRepository extends JpaRepository<Produto, Long> {
+	
+	
+	@Transactional(readOnly = true)
+	Produto findById(Long Id);
 
 }
